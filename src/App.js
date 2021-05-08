9d4f81a947eef2eb1a94fd5b78b6c2f2c898515f
mel-liow <mel.liow.94@gmail.com>
2021-05-08 08:23:00 +0100

    chore: add husky, prettier

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
-import './App.css';
+import "./App.css"
 
-const  App = () => {
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('here?', e.target)
-		// return fetch('/post', {
+const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("here?", e.target)
+    // return fetch('/post', {
     //   method: 'POST',
     //   headers: { 'Content-Type': 'application/json' },
     //   body: JSON.stringify({ data })
     // })
-	}
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-			<form onSubmit={handleSubmit}>
-				<label>
-					Enter Twitter handle:
-					<input type="text" name="name" />
-				</label>
-				<input type = "submit" value = "Submit" />
-			</form>
-
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter Twitter handle:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
