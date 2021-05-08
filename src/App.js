6fe587b834f08f30b1cb5fe7b6cdcb5594be6a68
mel-liow <mel.liow.94@gmail.com>
2021-05-08 10:10:12 +0100

    feat: return data from endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 import "./App.css"
 
 const App = () => {
+  const [twitterHandle, setTwitterHandle] = useState("")
+
   const handleSubmit = (e) => {
+    const twitterHandle = e.target.value
+    setTwitterHandle(twitterHandle)
     e.preventDefault()
-    console.log("here?", e.target)
-    // return fetch('/post', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ data })
-    // })
+
+    return fetch("/twitter", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ data: twitterHandle }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data)
+      })
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <div>{twitterHandle}</div>
         <form onSubmit={handleSubmit}>
           <label>
             Enter Twitter handle:
-            <input type="text" name="name" />
+            <input
+              type="text"
+              name="twitter"
+              value={twitterHandle}
+              onChange={handleSubmit}
+            />
           </label>
           <input type="submit" value="Submit" />
         </form>
