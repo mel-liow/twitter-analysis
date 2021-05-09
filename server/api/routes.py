395420a7773ed55d4ae9d0c504fe7ee5b0af76ed
mel-liow <mel.liow.94@gmail.com>
2021-05-09 21:41:03 +0100

    chore: convert tabs to spaces

--- server/api/routes.py
+++ server/api/routes.py
@@ -20,46 +20,46 @@ twitter = Twython(app.config["TWITTER_KEY"], app.config["TWITTER_SECRET"])
 
 @app.route('/twitter', methods=['POST'])
 def get_data():
-	data = request.json
-	twitterHandle = data['twitterHandle']
+  data = request.json
+  twitterHandle = data['twitterHandle']
 
-	#Get timeline 
-	user_timeline=twitter.get_user_timeline(screen_name=twitterHandle,count=1) 
-	last_id = user_timeline[0]['id']-1
-	batch = twitter.get_user_timeline(screen_name=twitterHandle,count=200, max_id=last_id)
-	user_timeline.extend(batch)
+  #Get timeline 
+  user_timeline=twitter.get_user_timeline(screen_name=twitterHandle,count=1) 
+  last_id = user_timeline[0]['id']-1
+  batch = twitter.get_user_timeline(screen_name=twitterHandle,count=200, max_id=last_id)
+  user_timeline.extend(batch)
 
-	#Extract textfields from tweets
-	tweets = [tweet['text'] for tweet in user_timeline]
-	
-	#Clean up words
-	words_no_punct = remove_punct(tweets)
-	words_tokenized = tokenization(words_no_punct)
-	words_no_stopwords = remove_stopwords(words_tokenized)
-	words_lemmatized = lemmatizer(words_no_stopwords)
+  #Extract textfields from tweets
+  tweets = [tweet['text'] for tweet in user_timeline]
+  
+  #Clean up words
+  words_no_punct = remove_punct(tweets)
+  words_tokenized = tokenize(words_no_punct)
+  words_no_stopwords = remove_stopwords(words_tokenized)
+  words_lemmatized = lemmatize(words_no_stopwords)
 
-	fdist = FreqDist(words_lemmatized)
-	frequent_words = json.dumps(fdist.most_common(50))
-	return frequent_words
+  fdist = FreqDist(words_lemmatized)
+  frequent_words = json.dumps(fdist.most_common(50))
+  return frequent_words
 
 
-def remove_punct(text):
-	text = ''.join(text)
-	text = re.sub(r'http\S+', '', text)
-	text = re.sub(r"\\[a-z][a-z]?[0-9]+", '', text)
-	text = re.sub('[^A-Za-z ]+', '', text)
-	return text
+def remove_punct(words):
+  words = ''.join(words)
+  words = re.sub(r'http\S+', '', words)
+  words = re.sub(r"\\[a-z][a-z]?[0-9]+", '', words)
+  words = re.sub('[^A-Za-z ]+', '', words)
+  return words
 
-def tokenization(text):
-	text = text.split(" ")
-	return text
+def tokenize(words):
+  words = words.split(" ")
+  return words
 
-def remove_stopwords(text):
-	text = [w for w in text if len(w) > 2]
-	text = [w.lower() for w in text]
-	text = [word for word in text if word not in stopword]
-	return text
+def remove_stopwords(words):
+  words = [w for w in words if len(w) > 2]
+  words = [w.lower() for w in words]
+  words = [word for word in words if word not in stopword]
+  return words
 
-def lemmatizer(text):
-  text = [wn.lemmatize(word) for word in text]
-  return text
+def lemmatize(words):
+  words = [wn.lemmatize(word) for word in words]
+  return words
