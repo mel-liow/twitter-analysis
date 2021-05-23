58a15a6fc3e88349916ee6bb9e4bac4d34fa5014
mel-liow <mel.liow.94@gmail.com>
2021-05-23 16:40:55 +0100

    feat: split out preprocessing logic

--- server/api/preprocess.py
+++ server/api/preprocess.py
@@ -1,13 +1,14 @@
 import re
 from string import punctuation 
-import nltk
 from nltk.corpus import stopwords 
 from nltk.stem import WordNetLemmatizer
 
 class PreProcessTweets: 
 	def __init__(self):
 		stopword = stopwords.words('english')
-		self._stopwords = set(stopword + list(punctuation) +['AT_USER','URL'] )
+		self._stopwords = set(stopword)
+		self._lemmatizer = WordNetLemmatizer()
+		
 
 	def processTweets(self, tweets):
 		processed_tweets = []
@@ -16,11 +17,15 @@ class PreProcessTweets:
 		return processed_tweets
 
 	def _processTweet(self, tweet):
+
 		tweet = tweet.lower() # convert to lower-case
-		tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) # remove URLs
-		tweet = re.sub('@[^\s]+', 'AT_USER', tweet) # remove usernames
-		tweet = re.sub(r'#([^\s]+)', r'\1', tweet) # remove the # in #hashtag
+		tweet = re.sub(r'http\S+', '', tweet) # remove URLs
+		tweet = re.sub(r"\\[a-z][a-z]?[0-9]+", '', tweet)
+		tweet = re.sub('[^A-Za-z ]+', '', tweet) # only allow letters
 		tweet = tweet.split(" ") # tokenize tweets
+		tweet = [word for word in tweet if len(word) > 2] # remove words with < 2 characters
+		tweet = [self._lemmatizer.lemmatize(word) for word in tweet] # lemmatize tweets
+
 		return [word for word in tweet if word not in self._stopwords]
 
 	def lemmatizeWords(self, tweet):
--- server/api/routes.py
+++ server/api/routes.py
@@ -1,65 +1,30 @@
 from flask import request
-import numpy as np
+from .preprocess import PreProcessTweets
 from twython import Twython
-import re
-import string
-import nltk
-import json
-
 from nltk import FreqDist
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('wordnet')
-
-wn = nltk.WordNetLemmatizer()
-stopword = nltk.corpus.stopwords.words('english')
-
 from api import app
+
+import json
 
 twitter = Twython(app.config["TWITTER_KEY"], app.config["TWITTER_SECRET"])
 
+
 @app.route('/twitter', methods=['POST'])
 def get_data():
-  data = request.json
-  twitterHandle = data['twitterHandle']
+	data = request.json
+	twitterHandle = data['twitterHandle']
 
-  #Get timeline 
-  user_timeline=twitter.get_user_timeline(screen_name=twitterHandle,count=1) 
-  last_id = user_timeline[0]['id']-1
-  batch = twitter.get_user_timeline(screen_name=twitterHandle,count=200, max_id=last_id)
-  user_timeline.extend(batch)
+	#Get timeline 
+	tweets = twitter.get_user_timeline(screen_name=twitterHandle,count=1) 
+	last_id = tweets[0]['id']-1
+	batch = twitter.get_user_timeline(screen_name=twitterHandle,count=200, max_id=last_id)
+	tweets.extend(batch)
+	
+	tweetProcessor = PreProcessTweets()
 
-  #Extract textfields from tweets
-  tweets = [tweet['text'] for tweet in user_timeline]
-  
-  #Clean up words
-  words_no_punct = remove_punct(tweets)
-  words_tokenized = tokenize(words_no_punct)
-  words_no_stopwords = remove_stopwords(words_tokenized)
-  words_lemmatized = lemmatize(words_no_stopwords)
+	processedTweets = tweetProcessor.processTweets(tweets)
+	words = [word for tweet in processedTweets for word in tweet]
 
-  fdist = FreqDist(words_lemmatized)
-  frequent_words = json.dumps(fdist.most_common(50))
-  return frequent_words
-
-
-def remove_punct(words):
-  words = ''.join(words)
-  words = re.sub(r'http\S+', '', words)
-  words = re.sub(r"\\[a-z][a-z]?[0-9]+", '', words)
-  words = re.sub('[^A-Za-z ]+', '', words)
-  return words
-
-def tokenize(words):
-  words = words.split(" ")
-  return words
-
-def remove_stopwords(words):
-  words = [w for w in words if len(w) > 2]
-  words = [w.lower() for w in words]
-  words = [word for word in words if word not in stopword]
-  return words
-
-def lemmatize(words):
-  words = [wn.lemmatize(word) for word in words]
-  return words
+	fdist = FreqDist(words)
+	frequent_words = json.dumps(fdist.most_common(50))
+	return frequent_words
