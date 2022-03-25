7a6890aacbbf769b8d7b3513200b62e8a901e75a
mel-liow <mel.liow.94@gmail.com>
2022-03-25 07:57:36 -0700

    style: linter

--- server/api/analysis.py
+++ server/api/analysis.py
@@ -4,33 +4,35 @@ import nltk
 from collections import defaultdict, Counter
 from joblib import load
 
-nltk.download('vader_lexicon')
+nltk.download("vader_lexicon")
 
 pipeline = load("../ml_model/tweet_classification.joblib")
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-class SentimentAnalyser: 
 
-	def scoreTweets(self, tweets):
+class SentimentAnalyser:
+    def scoreTweets(self, tweets):
 
-		score_count = defaultdict(int)
+        score_count = defaultdict(int)
 
-		scores = [SentimentIntensityAnalyzer().polarity_scores(tweet['text']) for tweet in tweets]
-		
-		for score in scores:
-			if score['compound'] > 0.0:
-				score_count['pos'] += 1
-			elif score['compound'] < 0.0:
-				score_count['neg'] += 1
-			else:
-				score_count['neu'] += 1
-		
-		return score_count
+        scores = [
+            SentimentIntensityAnalyzer().polarity_scores(tweet["text"])
+            for tweet in tweets
+        ]
 
-	def getSentiment(self, tweets):
+        for score in scores:
+            if score["compound"] > 0.0:
+                score_count["pos"] += 1
+            elif score["compound"] < 0.0:
+                score_count["neg"] += 1
+            else:
+                score_count["neu"] += 1
 
-		scores = pipeline.predict([tweet['text'] for tweet in tweets]) 
-		return Counter(scores)
+        return score_count
 
+    def getSentiment(self, tweets):
 
+        scores = pipeline.predict([tweet["text"] for tweet in tweets])
+        print("SCORES", scores)
+        return Counter(scores)
