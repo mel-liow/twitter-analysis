5eb3c7efefc687a35b24c9397a20f3e01e3a368f
mel-liow <mel.liow.94@gmail.com>
2022-03-25 10:03:38 -0700

    feat: add night mode button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react"
 import { WordCloud, SentimentChart } from "./components";
+import { Header, NavBar } from "./components/layout";
+
+import { Switch, Route } from 'react-router-dom'
+import { ThemeContext } from './context'
+
 import "./App.css"
 
 
@@ -7,6 +12,11 @@ const App = () => {
   const [twitterHandle, setTwitterHandle] = useState("")
   const [words, setWords] = useState([])
   const [sentiment, setSentiment] = useState([])
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  const setTheme = () => {
+    setIsDarkTheme(state => !state);
+  }
 
   const handleChange = (e) => {
     const twitterHandle = e.target.value
@@ -22,7 +32,7 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        let {words, scores} = data
+        let { words, scores } = data
 
         const scores_array = [scores['neg'], scores['neu'], scores['pos']]
         const cloud_words = JSON.parse(words).map((word) => {
@@ -38,8 +48,13 @@ const App = () => {
   }
 
   return (
-    <div className="app">
-      <header className="header">
+    <ThemeContext.Provider
+      value={{ isDarkTheme, toggleTheme: setTheme }}
+    >
+      <div className="app">
+        <Header>
+          <NavBar />
+        </Header>
         <form className="form" onSubmit={handleSubmit}>
           <label className="label">Enter Twitter handle below</label>
           <div>
@@ -53,16 +68,16 @@ const App = () => {
           </div>
         </form>
         <div className="twitterHandle">{twitterHandle}</div>
-      </header>
-      <div className="body">
-        <div className="section">
-          <WordCloud data={words} />
-        </div>
-        <div className="section">
-          <SentimentChart data={sentiment} />
+        <div className="body">
+          <div className="section">
+            <WordCloud data={words} />
+          </div>
+          <div className="section">
+            <SentimentChart data={sentiment} />
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeContext.Provider>
   )
 }
 
